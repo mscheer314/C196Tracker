52149df45c759c4a10e505a2aefc32b8902d3dac
mark scheer <mscheer314@gmail.com>
2019-12-11 22:41:25 -0600

    Fix term/course name input checking.

--- app/src/main/java/com/example/android/c196tracker/InputChecker.java
+++ app/src/main/java/com/example/android/c196tracker/InputChecker.java
@@ -1,11 +1,42 @@
 package com.example.android.c196tracker;
 
-import android.text.Editable;
-import android.text.TextUtils;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class InputChecker {
-    public static boolean isValidTermName(Editable editable) {
 
-        return !TextUtils.isEmpty(editable);
+    public static String checkNewItemInput(boolean isTerm, String name, String start, String end) {
+        String errorMessage = "";
+        if (name.length() == 0 && isTerm) {
+            errorMessage += "Please enter a term name.\n";
+        }
+        if (name == null && !isTerm) {
+            errorMessage += "Please enter a course name.\n";
+        }
+        if (start.equals("select date")) {
+            errorMessage += "Please select a start date.\n";
+        }
+        if (end.equals("select date")) {
+            errorMessage += "Please select an end date.\n";
+        }
+        if (!isValidCompareStartAndEndDates(start, end)) {
+            errorMessage += "The start date must be before the end date.\n";
+        }
+        return errorMessage;
+    }
+
+    public static boolean isValidCompareStartAndEndDates(String startString, String endString) {
+        try {
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM/dd/yyyy", Locale.US);
+            Date startDate = simpleDateFormat.parse(startString);
+            Date endDate = simpleDateFormat.parse(endString);
+
+            return startDate.compareTo(endDate) < 0;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
+
